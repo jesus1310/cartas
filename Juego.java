736a77e843d0a9c27abfb5996a916e6d056041e0
Jesus <jfr1013@gmail.com>
2016-02-05 12:48:15 +0100

    Creadas las clases Jugador y Juego

---
+++ Juego.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Juego here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Juego
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Juego
+     */
+    public Juego()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+}
---
+++ Jugador.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Jugador here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Jugador
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Jugador
+     */
+    public Jugador()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+}
